22dde89c61f8a9ada2f290741de140a4548a6036
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-02-04 18:10:25 +0200

    Home-work-1

---
+++ script.js
@@ -0,0 +1,43 @@
+let age = 14;
+console.log(age);
+
+let myName = "Rostyslav";
+console.log(myName);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Розум безсильний перед криком серця";
+console.log(myString);
+
+let myNumber = 10;
+myNumber = myNumber + 10;
+console.log(myNumber)
+
+let myNull = null
+console.log(myNull)
+
+let userName = prompt("Привіт, введи своє імя")
+if (userName !== null && userName !== '') {
+    alert("Привіт," + userName + "Ласково просимо!" )
+}
+else {
+    alert("скасовано або не введено імя")
+}
+console.log(userName)
+
+let myConfirm = confirm("Підтвердіть дію")
+if (myConfirm) {
+    alert("Дякую за підтвердження")
+}
+else {
+    alert("Дію скасовано")
+}
+alert("Небезпечно")
+let myAttention = confirm("Підтредіть дію")
+if (myAttention) {
+    alert("Дякую за підтвердеження")
+}
+else {
+    alert("підтвердження скасовано")
+}

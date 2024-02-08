e020caab6e9054f57cee519a2d8f5d18aba0eed5
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-02-08 16:57:33 +0200

    add

---
+++ script-2-lesson.js
@@ -0,0 +1,17 @@
+// Number
+const age = 18;
+
+const i = 5.2;
+
+// String
+
+const str = 'Hello world';
+
+// Boolean
+
+const isFail = true; //false
+
+const a = null; //null
+
+let b; // undefined
+console.log(b)

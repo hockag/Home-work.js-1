32f9eccf39886526dfa83da1e35ad34df417a8f0
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-02-09 22:22:04 +0200

    lesson-2

---
+++ script-3-lesson.js
@@ -0,0 +1,73 @@
+// const a = 10;
+//
+// const b = 3;
+//
+// console.log(a % b)
+//
+// console.log(5 % 2)
+//
+// console.log(8 % 3)
+//
+// console.log(8 % 4)
+
+// ділення з остачею
+
+// console.log(2 ** 2)
+// console.log(2 ** 3)
+// console.log(2 ** 4)
+//
+// const x = '5';
+// const y=10;
+// const z = 5;
+//
+// console.log('x > y', x > y);
+// console.log('x < y', x < y);
+// console.log('x ==> y', x >= y);
+// console.log('x <== y', x <= y);
+// console.log('x === z:', x === z);
+// console.log('x !== z:', x !== y);
+//
+//
+// let value = '45'
+//
+// value = Number(value)
+//
+// console.log(typeof value)
+//
+// const data = +prompt('Скільки вам років', "")
+// console.log(typeof data)
+//
+// let t = '18'
+//
+// console.log(t)
+
+// const isFail = true;
+//
+// console.log(+isFail)
+
+const apple = '50';
+
+const oranges = '20';
+// console.log(apple + oranges);
+// console.log(Number(apple) + Number(oranges));
+// console.log(+apple + +oranges);
+//
+// console.log(parseInt(apple));
+// console.log(parseFloat(oranges));
+//
+// console.log(isNaN(apple));
+//
+// const a = 10 / 3;
+//
+// console.log(a.toFixed(2))
+//
+// console.log(Math.floor(2.9));
+// console.log(Math.ceil(3.1));
+// console.log(Math.round(1.5));
+// console.log(Math.round(1.6));
+// console.log(Math.round(1.3));
+//
+// console.log(Math.max(4.7, 582, 95920924, 23456789))
+// console.log(Math.pow(2,4))
+// console.log(Math.random() * (10 - 1) + 1)
+

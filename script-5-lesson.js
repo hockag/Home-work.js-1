3c9c6e20e069be09291a0f4f536488d8d279b894
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-02-15 20:01:25 +0200

    lesson-5

---
+++ script-5-lesson.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const message = "Mango " + "is " + "happy ";
+console.log(message);
+
+let result;
+result = 5 + '5';
+console.log(result)
+console.log(typeof result);
+
+result = 5 + '5' + 5
+console.log(result)
+console.log(typeof result)
+
+result = 5 / '2';
+console.log(result);
+console.log(typeof result);
+
+result = 6 - '2';
+console.log(result)
+console.log(typeof result)
+
+const str = "Успіх - це не випадковість, а результат постійного старання";
+const author = 'наполеон Хілл';
+console.log(str.length);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.indexOf('випадковість'))
